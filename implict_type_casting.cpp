84e50ca69e6c4cdf6cba6685e22d8607cec6c02a
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2022-09-18 22:53:17 +0600

    example of implict type casting

---
+++ implict_type_casting.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main(){
+   //Implicit type casting means conversion of data types without losing its original meaning.
+  int x=10, y=3;
+  float  result;
+  result = (float)x/y;
+  cout<<result<<endl;
+}
