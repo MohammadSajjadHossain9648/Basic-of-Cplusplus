f60a566875fc15a9f292b2817724a4f0e3f397bb
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2022-09-19 23:52:47 +0600

    Show ASCII value of keyboard key

--- cin(input) in c++/keybaord_key_ASCII_code_with_cin-get().cpp
+++ cin(input) in c++/keybaord_key_ASCII_code_with_cin-get().cpp
@@ -2,6 +2,41 @@
 using namespace std;
 
 int main (){
-    int x = cin.get();
-    cout<< "ASCII Value of keyboard key is " << x <<endl;
+    
+    //give as an input in console
+    cout<< "Give a keyboard key :";
+    char x = cin.get();
+    cout<< "ASCII Value of keyboard key "<< x <<" is " << int(x) <<endl;
+                            //or
+//    cout<< "Give a keyboard key :";
+//    int x = cin.get();
+//    cout<< "ASCII Value of keyboard key is " << x <<endl;
+
+
+    //initial  as an input in console
+    int y = 'h';
+    cout<< "ASCII Value of keyboard key 'h' is " << y <<endl;
+
+    char ch = 'h';
+    cout<< "ASCII Value of keyboard key "<< ch <<" is " << int(ch) <<endl;
+
+    // using escape sequence for horizontal tab and escape sequence can not see like keyboard key "<< character<<" is " <<.....
+    char character = ' \b ';
+    cout<< "ASCII Value of keyboard key is " << int(character) <<endl;
+    /*
+    The table below lists escape sequences of C++.
+
+    Escape Sequences	Characters
+    \b	                Backspace
+    \f	                Form feed
+    \n	                Newline
+    \r same as 'r'      Return
+    \t	                Horizontal tab
+    \v	                Vertical tab
+    \\	                Backslash
+    \'	                Single quotation mark
+    \"	                Double quotation mark
+    \?	                Question mark
+    \0	                Null Character
+    */
 }
