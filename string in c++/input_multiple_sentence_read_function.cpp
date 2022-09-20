e21f9f73039e87fb6ff04c681928fcf76ae49dd4
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2022-09-20 16:58:14 +0600

    Input multiple sentence by cin.read()

--- string in c++/input_multiple_sentence_read_function.cpp
+++ string in c++/input_multiple_sentence_read_function.cpp
@@ -9,6 +9,10 @@ int main()
     cout<<"Input multiple sentence: ";
     cin.read(c, 20);
     cout<<"Enter sentence by char variable: "<< c <<endl;
+    
+    //note: cin.read never count space/tab/enter button to stop.
+    //take char whatever length is initialize in cin.read()
+    //count char with whitespace only for length not count newline
 
     //if cin.read(c, 10);
     //Input a sentence: welcome to code
