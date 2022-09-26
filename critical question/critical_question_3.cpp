b4de9ddebc911450ab59bf155c8814d5914cc2bb
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2022-09-26 14:26:43 +0600

    Imagine the output

---
+++ critical question/critical_question_3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a =20;
+
+    if(a==20){
+        cout<<"Twenty";
+    }
+    if(a>0){
+        cout<<"Positive";
+    }
+    else{
+        cout<<"Negative";
+    }
+
+}
