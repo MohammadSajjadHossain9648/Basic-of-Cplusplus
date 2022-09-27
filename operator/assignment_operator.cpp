4a38a2066d7fe9c833c8e68e275679b5538eb32f
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2022-09-27 23:09:13 +0600

    assignment operator in c++

---
+++ operator/assignment_operator.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+// Assignment Operators (=, +=, -=, *=, /=, %=)
+    cout<<"Assignment Operators (=, +=, -=, *=, /=, %=)"<<endl<<endl;
+
+    int x = 5;
+    cout << "Initial value of x is " << x <<endl;
+
+    cout << "x += 5 gives :" << (x += 5) <<endl;
+
+    cout << "x -= 5 gives : " << (x -= 5) <<endl;
+
+    cout << "x *= 5 gives :" << (x *= 5) <<endl;
+
+    cout << "x /= 5 gives : " << (x /= 5) <<endl;
+
+    cout << "x %= 5 gives : " << (x %= 5) <<endl;
+
+    // output:
+    //    Assignment Operators (=, +=, -=, *=, /=, %=)
+    //
+    //    Initial value of x is 5
+    //    x += 5 gives :10
+    //    x -= 5 gives : 5
+    //    x *= 5 gives :25
+    //    x /= 5 gives : 5
+    //    x %= 5 gives : 0
+}
+
