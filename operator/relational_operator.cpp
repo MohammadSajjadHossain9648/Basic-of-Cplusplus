e56b84d2f2dd6cb2870ad7bd18fdff19f24fc95f
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2022-09-28 12:56:54 +0600

    relational operator in c++

--- operator/relational_operator.cpp
+++ operator/relational_operator.cpp
@@ -12,57 +12,58 @@ int main()
 
     cout<<"a = "<<a<<", b = "<<b<<endl<<endl;
 
-    // greater than example
+   // greater than example
+    cout<<"a > b: ";
     if (a > b)
-        cout<<"a is greater than b\n"<<endl;
+        cout<<"a is greater than b"<<endl;
     else
-        cout<<"a is less than or equal to b\n"<<endl;
+        cout<<"a is less than or equal to b"<<endl;
 
     // greater than equal to
+    cout<<"a >= b: ";
     if (a >= b)
-        cout<<"a is greater than or equal to b\n"<<endl;
+        cout<<"a is greater than or equal to b"<<endl;
     else
-        cout<<"a is lesser than b\n"<<endl;
+        cout<<"a is lesser than b"<<endl;
 
     // less than example
+    cout<<"a < b: ";
     if (a < b)
-        cout<<"b is greater than a\n"<<endl;
+        cout<<"a is less than b"<<endl;
     else
-        cout<<"b is less than or equal to a\n"<<endl;
+        cout<<"a is greater than or equal to b"<<endl;
 
     // lesser than equal to
+    cout<<"a <= b: ";
     if (a <= b)
-        cout<<"b is greater than or equal to a\n"<<endl;
+        cout<<"a is lesser than or equal to b"<<endl;
     else
-        cout<<"b is less than a\n"<<endl;
+        cout<<"a is greater than b"<<endl;
 
     // equal to
+    cout<<"a == b:";
     if (a == b)
-        cout<<"a is equal to b\n"<<endl;
+        cout<<"a is equal to b"<<endl;
     else
-        cout<<"a and b are not equal\n"<<endl;
+        cout<<"a and b are not equal"<<endl;
 
     // not equal to
+    cout<<"a != b:";
     if (a != b)
-        cout<<"a is not equal to b\n"<<endl;
+        cout<<"a is not equal to b"<<endl;
     else
-        cout<<"a is equal b\n"<<endl;
+        cout<<"a is equal b"<<endl;
 
     //output:
     //    Relational Operators (==, !=, >, >=, <, <=)
     //
     //    a = 11, b = 5
     //
-    //    a is greater than b
-    //
-    //    a is greater than or equal to b
-    //
-    //    b is less than or equal to a
-    //
-    //    b is less than a
-    //
-    //    a and b are not equal
-    //
-    //    a is not equal to b
+    //    a > b: a is greater than b
+    //    a >= b: a is greater than or equal to b
+    //    a < b: a is greater than or equal to b
+    //    a <= b: a is greater than b
+    //    a == b:a and b are not equal
+    //    a != b:a is not equal to b
 }
 
