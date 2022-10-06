7a077fcd0e33242d2d3aa67c870d700851c03af2
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2022-10-06 16:41:52 +0600

    cout in c++

---
+++ cout(output) in c++/cout in c++.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"introduction for cout"<<endl<<endl;
+
+
+    cout<<"**** print datatype value ****"<<endl<<endl;
+    int a = 123;
+    char b = 'a';
+    string c = "i am new coder.";
+    float d = 12.24;
+    double e = 25.633333;
+
+    cout<<"int :"<< a <<endl;
+    cout<<"char :"<< b <<endl;
+    cout<<"string :"<< c <<endl;
+    cout<<"float :"<< d <<endl;
+    cout<<"double :"<< e <<endl<<endl;
+
+
+    cout<<"**** print special character value by using \****"<<endl<<endl;
+    string car = "FFerrarii";
+    cout<<"string car = \" "<<car<<" \" "<<endl<<endl;
+
+
+    cout<<"**** print many lines in one row by using comma operator ****"<<endl<<endl;
+    cout<<"I ",
+    cout<<"am ",
+    cout<<"new ",
+    cout<<"coder."<<endl<<endl;
+
+
+    cout<<"**** print arithmetic operation ****"<<endl<<endl;
+    cout<<"10+15 = "<<(10+15)<<endl;
+
+
+    //output:
+    //    introduction for cout
+    //
+    //    **** print datatype value ****
+    //
+    //    int :123
+    //    char :a
+    //    string :i am new coder.
+    //    float :12.24
+    //    double :25.6333
+    //
+    //    **** print special character value by using ****
+    //
+    //    string car = " FFerrarii "
+    //
+    //    **** print many lines in one row by using comma operator ****
+    //
+    //    I am new coder.
+    //
+    //    **** print arithmetic operation ****
+    //
+    //    10+15 = 25
+
+}
