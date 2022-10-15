cc149f2d2062f6845a3257a345a1de7e9fe7e6cf
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2022-10-15 20:00:23 +0600

    use of library function in c++

---
+++ function/basic/library_function.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main()
+{
+    /*
+        to know more: https://www.geeksforgeeks.org/functions-in-cpp/
+                      https://www.programiz.com/cpp-programming/function
+    */
+
+    double number = 25.0, squareRoot;
+
+    // sqrt() is a library function to calculate the square root
+    squareRoot = sqrt(number);
+
+    cout<<"square root of "<<number<<" is "<<squareRoot<<endl;
+
+    //output:
+    // square root of 25 is 5
+
+}
