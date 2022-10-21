c45a29873b070754778101bfa4011a6097cf789c
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2022-10-21 21:26:15 +0600

    return array from function by using auto array method

---
+++ array of int, char, string/basic/return an array from function/return_an_array_from_function_by_using_auto_array_method.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<array>
+using namespace std;
+/*
+    to know more: https://java2blog.com/return-array-from-function-cpp/
+                  https://codeforwin.org/2017/12/pass-return-array-function-c.html
+                  https://devenum.com/how-to-return-an-array-from-a-function-in-c/
+                  https://linuxhint.com/return-array-from-function-c/
+                  https://www.digitalocean.com/community/tutorials/return-array-in-c-plus-plus-function
+                  https://www.geeksforgeeks.org/how-to-return-a-local-array-from-a-cpp-function/
+*/
+/*
+    Using std:: array with auto method include header #include<array>
+    C++ also offers another choice for returning an array from a function – std:: array.
+    It is a structure template that wraps a regular array and it also provides extra methods
+    like size() & empty(). We can return the array name to return the whole array at the time
+    of the function call. We also need to include the header file ‘array’ to use this method.
+*/
+
+auto function() //function declared with return type array<>
+{
+    array<int, 10> arr; //array declared
+
+    int len = sizeof(arr)/sizeof(int);
+
+    for(int i=0; i<len; i++)
+    {
+        arr[i] = i;
+    }
+    return arr;
+}
+
+int main()
+{
+    auto arr = function(); //function calling
+
+    int len = sizeof(arr)/sizeof(int);
+
+    cout<<"arr[]: ";
+    for(int i=0; i<len; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+
+
+    //output: arr[]: 0 1 2 3 4 5 6 7 8 9
+}
