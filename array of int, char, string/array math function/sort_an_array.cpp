59c0731d59ab64135c6bf93d631130e539f7c6fc
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2022-11-01 15:24:33 +0600

    sort an array using sort function

--- array of int, char, string/array math function/sort_an_array.cpp
+++ array of int, char, string/array math function/sort_an_array.cpp
@@ -3,13 +3,16 @@
 using namespace std;
 /*
     to know more: https://www.geeksforgeeks.org/sort-c-stl/
+                  https://www.digitalocean.com/community/tutorials/sort-in-c-plus-plus      
 */
 int main()
 {
     int array[]= {1,7,5,4,3,6,2,8};
-
+    int array2[]= {1,7,5,4,3,6,2,8};
+    
     //compute the size
-    int size=sizeof(array)/sizeof(array[0]);
+    int size=sizeof(array)/sizeof(int);
+    int size2=sizeof(array2)/sizeof(int);
 
     //print the array
     cout<<"array[] = {}: ";
@@ -18,10 +21,22 @@ int main()
         cout<<array[i]<<"  ";
     }
 
+    //Sorting in Ascending Order
     sort(array, array+size);
+    
+    //Sorting in Descending Order
+    sort(array2, array2+size2, greater<int>());
 
-    //print the reverse array
-    cout<<endl<<"sort array: ";
+    //print the reverse array in Ascending Order
+    cout<<endl<<"sort array in Ascending Order: ";
+    for(int i=0; i<size; i++)
+    {
+        cout<<array[i]<<"  ";
+    }
+    cout<<endl;
+    
+    //print the reverse array in Descending Order
+    cout<<endl<<"sort array in Descending Order: ";
     for(int i=0; i<size; i++)
     {
         cout<<array[i]<<"  ";
@@ -30,5 +45,6 @@ int main()
 
     //output:
     //    array[] = {}: 1  7  5  4  3  6  2  8
-    //    sort array: 1  2  3  4  5  6  7  8
+    //    sort array in Ascending Order: 1  2  3  4  5  6  7  8
+    //    sort array in Descending Order: 8  7  6  5  4  3  2  1
 }
