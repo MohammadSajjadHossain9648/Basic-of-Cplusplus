c2983fa4b1e3e96bfffca0f1956b2c528e1dc83b
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2022-11-08 18:44:00 +0600

    for loop critical question 3

---
+++ critical question/loop/for_loop_critical_question_3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for(int i=0;i<=15;i++){
+        cout<<i<<" ";
+        if( i&1 ){
+            continue;
+        }
+        i++;
+    }
+    cout<<endl;
+}
+
