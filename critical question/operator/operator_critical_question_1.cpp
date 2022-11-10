323384158aaa69bd449b0bd9c3ca12cb71ce8d88
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2022-11-10 23:24:01 +0600

    operator critical question 1

---
+++ critical question/operator/operator_critical_question_1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a=10, b=2;
+    if(++a)
+    {
+        cout<<b<<endl;
+    }
+    else
+    {
+        cout<<++b<<endl;
+    }
+}
