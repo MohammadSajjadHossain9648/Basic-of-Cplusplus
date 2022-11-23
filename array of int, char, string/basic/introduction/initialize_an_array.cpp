f38d13b40c198bfe17eafa0d867155c16a0ee5c1
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2022-11-23 15:12:19 +0600

    initialize an array

--- array of int, char, string/basic/introduction/initialize_an_array.cpp
+++ array of int, char, string/basic/introduction/initialize_an_array.cpp
@@ -8,6 +8,8 @@ using namespace std;
                   https://www.geeksforgeeks.org/arrays-in-c-cpp/
                   https://thispointer.com/how-to-initialize-an-array-with-same-value-in-c/#:~:text=The%20very%20first%20and%20the,to%20give%20an%20initializer%20list.&text=int%20arr%5B%5D%20%3D%20%7B%2010%2C,%2C%2010%2C%2010%2C%2010%20%7D%3B
                   https://www.techiedelight.com/initialize-elements-array-same-value-c-cpp/
+                  https://www.geeksforgeeks.org/multidimensional-arrays-c-cpp/
+                  https://www.scaler.com/topics/two-dimensional-array-in-cpp/
 */
 
 //define macros
@@ -20,7 +22,8 @@ using namespace std;
 
 int main()
 {
-// input an array from user
+// One-Dimensional Array:
+    // input an array from user
     char ch[100]; //same as string ch; cause string is same type of char array
     int len;
     cout<<"\nenter the character length: ";
@@ -30,7 +33,7 @@ int main()
     //it is efficient for single char input
     //cin>>ch; //it take space -> len = 5, input =  tige r, output = tige
 
-    
+
     for(int i=0; i<len; i++)
     {
         cin>>ch[i]; //it take space -> len = 5, input =  tige r, output = tiger
@@ -40,12 +43,12 @@ int main()
 
     //or can be print
     cout<<"\nchar ch[] = ";
-    for(int i=0; i<len; i++) 
+    for(int i=0; i<len; i++)
     {
         cout<<ch[i]<<" ";
     }
     cout<<endl;
-    
+
     /*output:
     enter the character length: 10
     i am new here
@@ -54,9 +57,8 @@ int main()
 
     char ch[] = i a m n e w h e r e
     */
-    
-    
-// One-Dimensional Array:
+
+
     // initialising an array -> data_type variable_name[size of array]
     int integer_num[] = { 1, 2, 3, 4, 5 };
     float float_num[] = { 1.2, 2.4, 3.6, 4.8, 5.10 };
@@ -78,6 +80,9 @@ int main()
 
     //output:
     //    One-Dimensional Array for any datatype: 1.22  2.44  3.66  4.88   5.1
+
+
+
 
 
 // Two-Dimensional Array:
@@ -109,22 +114,53 @@ int main()
         cout<<endl;
     }
 
+
+    //another method: input an 2d-array from user by Dynamic Allocation
+    int r2, c2;
+    cout<<"\nEnter row and column number: \n";
+    cin>>r2>>c2;
+
+    int** arr5 = new int*[r2];
+    cout<<endl<<"\nEnter Two-Dimensional Array value: "<<endl;
+    for(int row=0; row<r2; row++)
+    {
+        arr5[row] = new int[c2];
+        for(int column=0; column<c2; column++)
+        {
+            cin>>arr5[row][column];
+        }
+        cout<<endl;
+    }
+
+    //print row wise 2d-array
+    cout<<"\nTwo-Dimensional Array(row * column): "<<endl;
+    for(int row=0; row<r2; row++)
+    {
+        for(int column=0; column<c2; column++)
+        {
+            cout<<arr5[row][column]<<" ";
+        }
+        cout<<endl;
+    }
+
+
+
     //print column wise 2d-array
     cout<<"\nTwo-Dimensional Array(column * row): "<<endl;
-    for(int column=0; column<c; column++)
+    for(int column=0; column<c2; column++)
     {
-        for(int row=0; row<r; row++)
+        for(int row=0; row<r2; row++)
         {
-            cout<<arr3[row][column]<<" ";
+            cout<<arr5[row][column]<<" ";
         }
         cout<<endl;
     }
     /* can also write column wise 2d-array
-    for(int row=0; row<c; row++)
+    for(int row=0; row<c2; row++)
     {
-        for(int column=0; column<r; column++)
+        for(int column=0; column<r2; column++)
         {
-            cout<<arr3[column][row]<<" ";
+            cout<<arr5[column][row]<<" ";
         }
         cout<<endl;
     }
@@ -133,13 +169,21 @@ int main()
 
 
     //initialize Two-Dimensional Array
-    int arr4 [3][2] = { {1, 2}, {3, 4}, {5, 6} }; //same as -> int arr3 [3][2] = {1, 2, 3, 4, 5, 6};
+    int arr4 [3][2] = { {1, 2}, {3, 4}, {5, 6} }; //same as -> int arr3 [3][2]/arr3 [][2] = {1, 2, 3, 4, 5, 6};
+
+    //get the length of row and column
+    int r1 = sizeof(arr4)/sizeof(arr4[0]); //indicates row length
+    int c1 = sizeof(arr4[0])/sizeof(int); //indicates column length
+    /* same as above
+    int r1 = sizeof(arr4)/sizeof(arr4[0]); //indicates row length
+    int c1 = sizeof(arr4[0])/sizeof(arr4[0][0]); //indicates column length
+    */
 
     //print row wise 2d-array
     cout<<"\nTwo-Dimensional Array(row * column): "<<endl;
-    for(int row=0; row<3; row++)
+    for(int row=0; row<r1; row++)
     {
-        for(int column=0; column<2; column++)
+        for(int column=0; column<c1; column++)
         {
             cout<<arr4[row][column]<<" ";
         }
@@ -148,18 +192,18 @@ int main()
 
     //print column wise 2d-array
     cout<<"\nTwo-Dimensional Array(column * row): "<<endl;
-    for(int column=0; column<2; column++)
+    for(int column=0; column<c1; column++)
     {
-        for(int row=0; row<3; row++)
+        for(int row=0; row<r1; row++)
         {
             cout<<arr4[row][column]<<" ";
         }
         cout<<endl;
     }
     /* can also write column wise 2d-array
-    for(int row=0; row<c; row++)
+    for(int row=0; row<c1; row++)
     {
-        for(int column=0; column<r; column++)
+        for(int column=0; column<r1; column++)
         {
             cout<<arr3[column][row]<<" ";
         }
@@ -168,6 +212,15 @@ int main()
     */
 
     //output:
+    //    Enter row and column number:
+    //    3
+    //    4
+    //
+    //
+    //    Enter Two-Dimensional Array value:
+    //    1 2 3 4 5 6 7 8 9 10 11 12
+    //
+    //
     //    Enter row and column number:
     //    3
     //    4
@@ -197,6 +250,9 @@ int main()
     //    Two-Dimensional Array(column * row):
     //    1 3 5
     //    2 4 6
+
+
+
 
 //Multidimensional Array:
     int array[5][10][4];
