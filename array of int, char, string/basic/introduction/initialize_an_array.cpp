8ea93298522a087a421ec326b20f99f937d580cf
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2022-11-27 22:03:01 +0600

    initialize an array

--- array of int, char, string/basic/introduction/initialize_an_array.cpp
+++ array of int, char, string/basic/introduction/initialize_an_array.cpp
@@ -317,5 +317,18 @@ int main()
 //Multidimensional Array:
     int array[2][3][2] = {  { { 0, 1 }, { 2, 3 }, { 4, 5 } },
                             { { 6, 7 }, { 8, 9 }, { 10, 11 } }  };
+    
+    cout<<"\narr6[2][3][2]: \n";
+    for (int i = 0; i < 2; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            for (int k = 0; k < 2; ++k) {
+                cout<<arr6[i][j][k]<<" ";
+            }
+            cout<<endl;
+        }
+        cout<<endl<<endl;
+    }
+    
+    
 
 }
