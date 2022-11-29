29f74c027e1ba99e97cdae20b269feaeb9721d4e
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2022-11-29 23:11:55 +0600

    count all prime of first n natural numbers

---
+++ int/count_all_prime_of_first_n_natural_numbers2_using_function.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+/*
+    to know more: https://leetcode.com/problems/count-primes/
+*/
+int countPrimes(int num)
+{
+    int count = 0;
+    if(num < 2)
+    {
+        return 0;
+    }
+
+    for(int i=2; i<=num; i++)
+    {
+        bool prime = true;
+        for(int j=2; j<i; j++)
+        {
+            if(i%j == 0)
+            {
+                prime = false;
+                break;
+            }
+        }
+
+        if(prime)
+        {
+            count++;
+            cout<<"\nvalue: "<<i<<" -> count = "<<count;
+        }
+    }
+
+    return count;
+}
+
+int main()
+{
+    int num;
+    cout<<"enter an integer: ";
+    cin>>num;
+
+    int count = countPrimes(num);
+    cout<<"\ntotal number of prime numbers: "<<count;
+}
