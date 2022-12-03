808ec9814bb87294f024c750ac361c80ffb9ff77
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2022-12-03 23:16:46 +0600

    Search a 2D Matrix

---
+++ array of int, char, string/interview important array question/orginal/Search a 2D Matrix.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+using namespace std;
+/*
+    to know more: https://leetcode.com/problems/search-a-2d-matrix/
+*/
+int main()
+{
+    int arr[3][4] = {1,2,3,4,5,6,7,8,9,10,11,12};
+
+    int x;
+    cout<<"enter the search value: ";
+    cin>>x;
+
+    cout<<"\nint arr[3][4]:\n";
+    for(int row=0; row<3; row++)
+    {
+        for(int column=0; column<4; column++)
+        {
+            cout<<arr[row][column]<<" ";
+        }
+        cout<<endl;
+    }
+
+    bool found = 0;
+
+    for(int i=0; i<3; i++)
+    {
+        for(int j=0; j<4; j++)
+        {
+            if(x == arr[i][j])
+            {
+                cout<<"\nfound and position is in row "<<i<<" column "<<j;
+                found = 1;
+                break;
+            }
+        }
+    }
+
+    if(found == 0)
+    {
+        cout<<"\n"<<x<<" is not found in 2d-array";
+    }
+
+
+
+    //output:
+    //    enter the search value: 8
+    //
+    //    int arr[3][4]:
+    //    1 2 3 4
+    //    5 6 7 8
+    //    9 10 11 12
+    //
+    //    found and position is in row 1 column 3
+
+
+    /*
+    think, arr in linear way -> 1 2 3 4 5 6 7 8 9 10 11 12
+    we know, time complexity of linear search is O(n). here, n is the length of array.
+    but, when we use 2d-array or matrix, total length is row*col which is m*n. so that, O(mn)
+    time complexity: O(mn)
+    */
+
+}
