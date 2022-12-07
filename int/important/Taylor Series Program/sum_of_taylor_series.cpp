a6c1bd157f5b012f4d5c379773025cdd25ac8b26
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2022-12-07 20:33:58 +0600

    sum of taylor series

---
+++ int/important/Taylor Series Program/sum_of_taylor_series.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+/*
+    to know more: https://byjus.com/maths/taylor-series/
+                  https://www.studysmarter.co.uk/explanations/math/calculus/taylor-series/
+                  https://www.studytonight.com/c/programs/important-concepts/sum-of-taylor-series
+                  https://www.geeksforgeeks.org/program-to-efficiently-calculate-ex/
+                  https://www.geeksforgeeks.org/program-to-calculate-ex-by-recursion/
+                  https://dotnettutorials.net/lesson/taylor-series-using-recursion-in-c/
+*/
+/*
+    Below is a program on sum of taylor series.
+    (n+1)th term of taylor series is given by (x^n)/n! , where series starts with 1.
+
+    or can say that,
+    The value of Exponential Function e^x can be expressed using following Taylor Series.
+    e^x = 1 + x/1! + x^2/2! + x^3/3! + ......
+
+    The series can be re-written as
+    e^x = 1 + (x/1) (1 + (x/2) (1 + (x/3) (........) ) )
+*/
+
+float sumTaylor(int x, int n)
+{
+    float sum = 0;
+    int fact = 1;
+
+    for(int i=1; i<n; i++) //iteration not start with 0
+    {
+        fact = fact*i;
+        sum = sum + (pow(x,i)/fact);
+    }
+    sum = sum+1; //Since series starts with 1
+
+    return sum;
+
+    /*another way is
+    float sum = 1.0f; // initialize sum of series
+
+    for (int i = n - 1; i > 0; --i )
+        sum = 1 + x * sum / i;
+
+    return sum;
+    */
+}
+
+int main()
+{
+    int x, n;
+    cout<<"Enter the value of x in the series: ";
+    cin>>x;
+    cout<<"Enter the number of term in the series: ";
+    cin>>n;
+
+    float result = sumTaylor(x, n);
+    cout<<"\n\nThe sum of the taylor series is : "<<result<<endl;
+
+    //output:
+    //    Enter the value of x in the series: 4
+    //    Enter the number of term in the series: 15
+    //    e^x = 54.59788
+
+
+    /*
+    Time Complexity: O(n)
+    Auxiliary Space: O(1), since no extra space has been taken.
+    */
+}
