5c01f6a7c320e6ec004d39e7709809259c972f50
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2022-12-13 15:27:07 +0600

    initialize char array

--- string in c++/string basic/word_initialized_variable_by_char.cpp
+++ string in c++/string basic/word_initialized_variable_by_char.cpp
@@ -5,12 +5,16 @@ int main()
 {
 // the array of char elements called x,y,z,myword can be initialized with a null-terminated sequence of characters by either one of these two statements:
     char z = 'v';
-    char x[] = { 'H', 'e', 'l', 'l', 'o', '\0' };
-//    or, char x[] = "hello";
-    char y[10] = "hello man";
-//    or, char y[10] = { 'H', 'e', 'l', 'l', 'o', ' ', 'm', 'a', 'n', '\0' }
+    
+    char x[] = { 'H', 'e', 'l', 'l', 'o', '\0' }; //same as, char x[] = "hello";
+    /* can write above char x[]:
+    char x[] = "hello"; or char * x = "hello";
+    but show error if write -> char * x = { 'H', 'e', 'l', 'l', 'o', '\0' };
+    */
+    
+    char y[10] = "hello man"; // char y[10] = { 'H', 'e', 'l', 'l', 'o', ' ', 'm', 'a', 'n', '\0' }
 
-//    char myword = { 'B', 'y', 'e', '\0' }; --> give an error, do it by this way
+    //char myword = { 'B', 'y', 'e', '\0' }; --> give an error, do it by this way
 
     char myword[4];
     myword[0]='B';
