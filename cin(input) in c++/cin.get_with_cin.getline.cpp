14d406360c37f471254dc75c9da17f586c1ff96a
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2022-12-13 18:12:50 +0600

    Issue occur when we use cin.getline after cin.get

--- cin(input) in c++/cin.get_with_cin.getline.cpp
+++ cin(input) in c++/cin.get_with_cin.getline.cpp
@@ -6,32 +6,32 @@ int main()
     int x;
     string name;
 
-    cout << "Type your favorite number: ";
+    cout<<"Type your favorite number: ";
 
-    // The cin statement uses the x and leaves the \n
-    // in the stream as garbage
-    cin >> x;
+    // The cin statement uses the x and leaves the \n in the stream as garbage
+    cin>>x;
 
-    cout << "Type your name : ";
+    cout<<"Type your name : ";
 
     // getline() reads \n and finish reading
     getline(cin, name);
 
     // In output only x will be displayed not name
-    cout << name << ", your favourite number is : "<< x;
+    cout<<name<<", your favourite number is : "<< x;
 
     //Type your favorite number: 7
     //Type your name : , your favourite number is : 7
 
 
-//    solve:
-//        cout << "Type your name: ";
-//
-//    // Usage of std::ws will extract all  the whitespace character
-//    getline(cin >> ws, name);
+    /*
+    solve the above problem by this two ways:
+    cout<<"Type your name: ";
+    // Usage of std::ws will extract all the whitespace character
+    getline(cin>>ws, name);
 
-                //or
-//        #include<limits>
-//        cin.ignore(numeric_limits<streamsize>::max(),'\n');
-//        cout << "Type your name: ";
+                or,
+    #include<limits>
+    cin.ignore(); or cin.ignore(numeric_limits<streamsize>::max(),'\n');
+    cout<<"Type your name: ";
+    */
 }
