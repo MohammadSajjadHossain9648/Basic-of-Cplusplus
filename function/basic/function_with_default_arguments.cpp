4a3b1a8400235b2586f8c9605f97af0c1e0ae951
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2022-12-27 18:34:21 +0600

    function with default arguments

--- function/basic/function_with_default_arguments.cpp
+++ function/basic/function_with_default_arguments.cpp
@@ -3,11 +3,26 @@ using namespace std;
 /*
     to know more: https://www.geeksforgeeks.org/functions-in-cpp/
 */
+/*
+    note: rightmost value must define or set as default argument. then you can set second rightmost value as default
+    argument.
+*/
 
 // here b = 20 is a default argument. if you sent no value of b, it works with default value as argument.
 int printNumbers(int a, int b = 20)
 {
     cout<<"a = "<< a <<endl, cout<<"b = "<< b <<endl<<endl;
+}
+
+//another example
+//show error -> void printArray(int *arr, int start=0, int end) -> see note above
+void printArray(int *arr, int start, int end=5)
+{
+    cout<<"\n\nint arr[] = ";
+    for(int i=start; i<end; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
 }
 
 int main()
@@ -26,4 +41,19 @@ int main()
     //    printNumbers(10, 40):
     //    a = 10
     //    b = 40
+
+
+//another example
+    int arr[] = {1,2,3,4,5,6,7,8,9,10};
+
+    printArray(arr, 0, 10);
+    printArray(arr, 0);
+
+
+
+    //output:
+    //
+    //    int arr[] = 1 2 3 4 5 6 7 8 9 10
+    //
+    //    int arr[] = 1 2 3 4 5
 }
