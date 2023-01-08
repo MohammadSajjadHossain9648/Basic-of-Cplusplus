7afd985e9e772bf7b8095c1b5e277172fd389b6d
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2023-01-08 19:39:58 +0600

    convert string to int / float / double

--- int/convert_string_to_int.cpp
+++ int/convert_string_to_int.cpp
@@ -20,7 +20,25 @@ int main()
     }
 
     cout<<"convert string to int: "<<number<<endl<<endl;
-    
+ 
+    //print all digits separately
+    cout<<"digits:\n";
+    for(auto i=0; i<digits.size(); i++)
+    {
+        cout<<digits[i] - '0'<<endl;  //here, (digits[i] - '0') -> string convert to int
+    }
+
+    //output:
+    //    Enter a string int: 153
+    //    convert string to int: 153
+    //
+    //    digits:
+    //    1
+    //    5
+    //    3
+
+
+
     
     string str;
     cout<<"Enter a string int: ";
@@ -33,6 +51,8 @@ int main()
     //    Enter a string int: 1.23/ 12/ 123 abc/ 123abc/ abc123
     //    convert string to int: 1/ 12/ 123/ 123/ show error
 
+    
+    
 //another example use sstream
     string str2;
     cout<<"Enter a string int: ";
